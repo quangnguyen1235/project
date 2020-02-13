0c4f7691aa56784167e9003278a2e19df8be4aa9
quangnguyen1235 <nguyendangquanghbt12b7@gmail.com>
2020-02-13 09:16:34 +0700

    bal

---
+++ codegym/bc/lession 2/sudung-toantu/site.js
@@ -0,0 +1,8 @@
+var inputWidth;
+var inputHeight;
+inputWidth=prompt('enter the width');
+inputHeight=prompt('enter the height');
+var Width=parseInt(inputWidth);
+var Height=parseInt(inputHeight);
+var area= Width*Height;
+document.write('the arena is:'+area);
---
+++ codegym/bc/lession1/site.js
@@ -0,0 +1,8 @@
+document.write('helele');
+console.log('hello');
+alert('hello');
+let a=5;
+let b=7;
+let c=a+b;
+document.write('<br>');
+document.write('a+b='+c);
